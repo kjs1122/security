ba5f9e89f5145d1b3ad22334672365cacea4a9c7
kimjisoo <kimjisoo3653@nate.com>
2022-05-12 00:01:16 +0900

    2. Entity Read

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package org.zerock.club.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,4 +18,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // 사용자 계정은 USER 1
+        auth.inMemoryAuthentication().withUser("user1")
+                // 1111 패스워드 인코딩 결과
+                .password("$2a$10$7Re3RunuMSscO8WtJjRCm.VHMCqBfgvXWBw.l8oy.CWOPVH20M0Sy")
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+
+        http.formLogin();
+        http.csrf().disable();
+        http.logout();
+    }
 }
